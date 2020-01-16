ae15deb1f81761c16926788726225b9c68a171f4
Gorbataras <gorbataras@gmail.com>
2020-01-15 20:06:07 -0500

    Separated JS2 and JS3; Created scripts folder

--- scripts/JS3.js
+++ scripts/JS3.js
@@ -1,16 +1,3 @@
-for (let index = 1; index <= 100; index++) {
-    if (index % 3 == 0 && index % 5 == 0) {
-        console.log("Hee Haw!");
-    } else if (index % 3 == 0) {
-        console.log("Hee!");
-    } else if (index % 5 == 0) {
-        console.log("Haw!");
-    } else {
-        console.log(index);
-    }
-
-}
-
 
 function heeHaw(num) {
     if (num == isNaN(num) || num <=0) {
