006b766ad8e5d5b370635bf168ede45633d33ea6
Gorbataras <gorbataras@gmail.com>
2020-01-22 22:24:55 -0500

    Added js3.html

--- scripts/JS3.js
+++ scripts/JS3.js
@@ -1,17 +1,17 @@
 
 function heeHaw(num) {
-    if (num == isNaN(num) || num <=0) {
-        console.log("Invalid Number");
+    if (isNaN(num) || num < 0) {
+        document.write("Invalid Number");
     } else {
         for (let index = 1; index <= num; index++) {
             if (index % 3 == 0 && index % 5 == 0) {
-                console.log("Hee Haw!");
+                document.write("Hee Haw!");
             } else if (index % 3 == 0) {
-                console.log("Hee!");
+                document.write("Hee!");
             } else if (index % 5 == 0) {
-                console.log("Haw!");
+                document.write("Haw!");
             } else {
-                console.log(index);
+                document.write(index);
             }
 
         }
